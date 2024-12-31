859735a192c83d49d9957a6eb0b6f3de1636bb9b
lynj <82319291+earlyoonj@users.noreply.github.com>
2024-12-31 10:36:09 -0800

    Refactor: Today | Manage variables, update done list UI #7

--- app/(screens)/today/page.tsx
+++ app/(screens)/today/page.tsx
@@ -15,6 +15,7 @@ import utc from 'dayjs/plugin/utc';
 import dayjs from 'dayjs';
 import Loader from '@/components/loader/Loader';
 import List from '@/app/_components/List';
+import { useMemo } from 'react';
 
 dayjs.extend(utc);
 
@@ -26,6 +27,73 @@ const Page = () => {
   const { data: notes, isFetching: isFetchingNotes } = useAtomValue(notesAtom);
   const { data: goals, isFetching: isFetchingGoals } = useAtomValue(goalsAtom);
 
+  const overdueArr = useMemo(() => {
+    return (
+      tasks?.filter(
+        (task) =>
+          task.status === 'todo' &&
+          task.date &&
+          dayjs(getDashDate(task.date)) < dayjs(getDashDate(new Date()))
+      ) || []
+    );
+  }, [today, tasks]);
+
+  const todoArr = useMemo(() => {
+    const eventArr =
+      events?.filter((event) => getDashDate(event.date) === getDashDate(today)) || [];
+    const noteArr =
+      notes?.filter((note) => getDashDate(note.date) === getDashDate(today)) || [];
+    const taskArr =
+      tasks?.filter(
+        (task) =>
+          task.status !== 'done' &&
+          task.status !== 'dismissed' &&
+          task.date &&
+          getDashDate(task.date) === getDashDate(today)
+      ) || [];
+
+    return [...eventArr, ...taskArr, ...noteArr];
+  }, [today, tasks, events, notes, goals]);
+
+  const doneArr = useMemo(() => {
+    return (
+      tasks?.filter(
+        (task) =>
+          (task.status === 'done' || task.status === 'dismissed') &&
+          task.date &&
+          getDashDate(task.date) === getDashDate(today)
+      ) || []
+    );
+  }, [today, tasks, events, notes, goals]);
+
+  const pinnedArr = useMemo(() => {
+    const goalArr =
+      goals?.filter(
+        (goal) => goal.isPinned && goal.status !== 'done' && goal.status !== 'dismissed'
+      ) || [];
+    const taskArr =
+      tasks?.filter(
+        (task) =>
+          task.status !== 'done' &&
+          task.status !== 'dismissed' &&
+          task.isPinned &&
+          (!task.date || getDashDate(task.date) !== getDashDate(today))
+      ) || [];
+
+    const eventArr =
+      events?.filter(
+        (event) =>
+          event.isPinned &&
+          event.date &&
+          getDashDate(event.date) &&
+          dayjs(getDashDate(event.date)!) >= dayjs(getDashDate(new Date())!)
+      ) || [];
+
+    const noteArr = notes?.filter((goal) => goal.isPinned) || [];
+
+    return [...goalArr, ...taskArr, ...eventArr, ...noteArr];
+  }, [today, tasks, events, notes, goals]);
+
   return (
     <div className="h-full p-14 flex flex-col gap-10">
       {/* Header */}
@@ -33,132 +101,47 @@ const Page = () => {
         <YearMonthNav />
         <DayNav />
       </div>
-      {!!tasks?.filter(
-        (task) =>
-          task.status === 'todo' &&
-          task.date &&
-          dayjs(getDashDate(task.date)) < dayjs(getDashDate(new Date()))
-      ).length && (
+      {!!overdueArr.length && (
         <List
           className="box-content w-full ml-[-1.5rem] [&_li]:text-base [&_svg]:text-xs py-3 px-6 bg-red-50 rounded-xl [&_h5]:text-red-400 [&_svg]:text-red-200 [&>div>svg]:text-red-400"
           title="Overdue Tasks"
-          items={
-            tasks?.filter(
-              (task) =>
-                task.status === 'todo' &&
-                task.date &&
-                dayjs(getDashDate(task.date)) < dayjs(getDashDate(new Date()))
-            ) || []
-          }
+          items={overdueArr}
           gap={2}
           isFolded={true}
         ></List>
       )}
-      <ul
-        className={`${
-          isFetchingTasks || isFetchingEvents || isFetchingNotes || isFetchingGoals
-            ? 'h-[80%]'
-            : ''
-        } space-y-6`}
-      >
-        {isFetchingTasks || isFetchingEvents || isFetchingNotes || isFetchingGoals ? (
-          <div className="w-full h-full flex justify-center items-center">
-            <Loader />
-          </div>
-        ) : (
-          <>
-            {events
-              ?.filter((event) => getDashDate(event.date) === getDashDate(today))
-              ?.map((event) => (
-                <ListItem key={event.id} {...event} />
-              ))}
-            {tasks
-              ?.filter((task) => {
-                if (
-                  task.status !== 'done' &&
-                  task.status !== 'dismissed' &&
-                  task.date &&
-                  getDashDate(task.date) === getDashDate(today)
-                ) {
-                  return true;
-                }
-              })
-              ?.map((task) => (
-                <ListItem key={task.id} {...task} />
-              ))}
-            {notes
-              ?.filter((note) => getDashDate(note.date) === getDashDate(today))
-              ?.map((note) => (
-                <ListItem key={note.id} {...note} />
-              ))}
-          </>
-        )}
-      </ul>
-      <List
-        title="Completed Tasks"
-        className="mt-2 [&_li]:text-base [&_svg]:text-xs"
-        items={
-          tasks?.filter(
-            (task) =>
-              (task.status === 'done' || task.status === 'dismissed') &&
-              task.date &&
-              getDashDate(task.date) === getDashDate(today)
-          ) || []
-        }
-        gap={6}
-        isFolded={true}
-      />
-      {!!(
-        goals
-          ?.filter(
-            (goal) =>
-              goal.isPinned && goal.status !== 'done' && goal.status !== 'dismissed'
-          )
-          ?.map((goal) => <ListItem key={goal.id} {...goal} />).length ||
-        tasks?.filter(
-          (task) =>
-            task.status !== 'done' &&
-            task.status !== 'dismissed' &&
-            task.isPinned &&
-            (!task.date || getDashDate(task.date) !== getDashDate(today))
-        ).length
-      ) && (
+      {!!todoArr.length && (
+        <ul
+          className={`${
+            isFetchingTasks || isFetchingEvents || isFetchingNotes || isFetchingGoals
+              ? 'h-[80%]'
+              : ''
+          } space-y-6`}
+        >
+          {isFetchingTasks || isFetchingEvents || isFetchingNotes || isFetchingGoals ? (
+            <div className="w-full h-full flex justify-center items-center">
+              <Loader />
+            </div>
+          ) : (
+            todoArr?.map((item) => <ListItem key={item.id} {...item} />)
+          )}
+        </ul>
+      )}
+      {!!doneArr.length && (
+        <List
+          title="Done"
+          className="mt-2 [&>div]:w-[108%] [&>div]:ml-[-4%] [&>div]:border-t-2 [&>div]:border-t-primary [&>div]:py-3 [&>div]:px-2 [&_li]:text-base [&_svg]:text-xs"
+          items={doneArr}
+          gap={2}
+          isFolded={false}
+          isRightSideArrow={true}
+        />
+      )}
+      {!!pinnedArr.length && (
         <ul className="box-content w-full ml-[-1.5rem] space-y-4 p-6 bg-gray-50 rounded-xl">
-          {goals
-            ?.filter(
-              (goal) =>
-                goal.isPinned && goal.status !== 'done' && goal.status !== 'dismissed'
-            )
-            ?.map((goal) => (
-              <ListItem key={goal.id} {...goal} />
-            ))}
-          {tasks
-            ?.filter(
-              (task) =>
-                task.status !== 'done' &&
-                task.status !== 'dismissed' &&
-                task.isPinned &&
-                (!task.date || getDashDate(task.date) !== getDashDate(today))
-            )
-            ?.map((task) => (
-              <ListItem key={task.id} {...task} />
-            ))}
-          {events
-            ?.filter(
-              (event) =>
-                event.isPinned &&
-                event.date &&
-                getDashDate(event.date) &&
-                dayjs(getDashDate(event.date)!) >= dayjs(getDashDate(new Date())!)
-            )
-            ?.map((event) => (
-              <ListItem key={event.id} {...event} />
-            ))}
-          {notes
-            ?.filter((goal) => goal.isPinned)
-            ?.map((note) => (
-              <ListItem key={note.id} {...note} />
-            ))}
+          {pinnedArr?.map((item) => (
+            <ListItem key={item.id} {...item} />
+          ))}
         </ul>
       )}
     </div>
--- app/_components/List.tsx
+++ app/_components/List.tsx
@@ -1,42 +1,56 @@
-import { ClassNameProps } from '@/types/className';
-import React, { ReactNode, useState } from 'react';
-import { IconType } from 'react-icons';
-import { FaChevronDown, FaChevronRight } from 'react-icons/fa6';
-import ListItem from './ListItem';
 import { EventType } from '@/store/event';
-import { TaskType } from '@/store/task';
-import { NoteType } from '@/store/note';
 import { GoalType } from '@/store/goals';
+import { NoteType } from '@/store/note';
+import { TaskType } from '@/store/task';
+import { ClassNameProps } from '@/types/className';
+import { useState } from 'react';
+import { FaChevronDown, FaChevronRight, FaChevronUp } from 'react-icons/fa6';
+import ListItem from './ListItem';
 
 interface ListProps extends ClassNameProps {
   title?: string;
   items: (GoalType | NoteType | TaskType | EventType)[];
   gap?: number;
   isFolded?: boolean;
+  isRightSideArrow?: boolean;
 }
 
-const List = ({ title, items, isFolded, gap, className }: ListProps) => {
+const List = ({ title, items, isFolded, gap, isRightSideArrow, className }: ListProps) => {
   const [hide, setHide] = useState(isFolded);
 
   return (
     <ul className={className}>
       {!!title && !!items?.length && (
         <div
-          className={`flex gap-2 items-center  ${hide ? '' : gap ? 'mb-' + gap : 'mb-4'} cursor-pointer`}
+          className={`flex gap-2 items-center  ${
+            hide ? '' : gap ? 'mb-' + gap : 'mb-4'
+          } ${isRightSideArrow ? 'justify-between' : ''} cursor-pointer`}
           onClick={() => {
             setHide((prev) => !prev);
           }}
         >
           {isFolded !== undefined &&
+            !isRightSideArrow &&
             (hide ? (
               <FaChevronRight className="text-[0.675rem]" />
             ) : (
               <FaChevronDown className="text-[0.675rem]" />
             ))}
           <h5 className="font-extrabold text-sm">{title}</h5>
+          {isFolded !== undefined &&
+            isRightSideArrow &&
+            (hide ? (
+              <FaChevronUp className="text-[0.675rem]" />
+            ) : (
+              <FaChevronDown className="text-[0.675rem]" />
+            ))}
         </div>
       )}
-      <li className={`space-y-6 text-lg font-light ${isFolded !== undefined ? 'ml-4' : ''}`}>
+      <li
+        className={`space-y-6 text-lg font-light ${
+          isFolded !== undefined && !isRightSideArrow ? 'ml-4' : ''
+        }`}
+      >
         {!hide && items?.map((item) => <ListItem key={item.id} {...item} />)}
       </li>
     </ul>
